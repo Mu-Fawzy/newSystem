ab2f06e091ddb27c3204bb0236759770923c7e5e
Mu-Fawzy <mrrmohamedfawzy@gmail.com>
2021-02-22 22:26:05 +0200

    19-2 commit

--- app/Http/Controllers/Dashboard/WorkitemsController.php
+++ app/Http/Controllers/Dashboard/WorkitemsController.php
@@ -96,8 +96,9 @@ class WorkitemsController extends Controller
      * @param  \App\Models\Workitem  $workitems
      * @return \Illuminate\Http\Response
      */
-    public function update(WorkitemRequest $request, Workitem $workitem)
+    public function update(Request $request, Workitem $workitem)
     {
+        return $request;
         $data = [];
         foreach(LaravelLocalization::getSupportedLanguagesKeys() as $localeCode){
             $data['name'][$localeCode] = $request["name.".$localeCode];
