1423b8c922526bfc5838a488c6bec4a8af58241d
Mu-Fawzy <mrrmohamedfawzy@gmail.com>
2021-02-22 21:09:31 +0200

    16 commit

--- app/Http/Controllers/Dashboard/WorkitemsController.php
+++ app/Http/Controllers/Dashboard/WorkitemsController.php
@@ -61,7 +61,6 @@ class WorkitemsController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
         $data = array();
         foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties){
             $data['name'][$localeCode] = $request["name_".$localeCode];
